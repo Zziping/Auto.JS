af9a2c6cb1486438198319013900df2b07d200a1
Zziping <Z_ziping@163.com>
2022-11-14 17:56:45 +0800

    First commit.

---
+++ AutoJS学习/Project/Demo1/js入门.js
@@ -0,0 +1,5 @@
+if(test("微信").findOne(3000)){
+    console.log("true");
+}else{
+    console.log("false");
+}
---
+++ conn.js
@@ -0,0 +1,6 @@
+home();
+text("微信").waitFor();
+sleep(1000)
+click("微信")
+log("clicked")
+toast("微信已开启")
